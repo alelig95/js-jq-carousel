89e958839ab4602b0c1900e36e72f250829bc319
Alessandro Liggieri <smmb@live.it>
2021-01-13 17:52:06 +0100

    Esercizio completo (senza pallini funzionanti)

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-  $('.next > i').click(function () {
-    $('.images > .active').removeClass('active').next().addClass('active');
+  $('.next').click(function () {
+    // creiamo una variabile per indicare l'immagine attualmente visualizzata:
+    var activeImg = $('.images img.active');
+    activeImg.removeClass('active');
+    // creiamo un if per capire se l'immagine visualizzata è l'ultima:
+    if (activeImg.hasClass('last')) {
+      // Se è l'ultima, allora l'immagine visualizzata successiva sarà la prima
+      var firstImg = $('.images img.first');
+      firstImg.addClass('active');
+    } else {
+      // Se non è l'ultima passa semplicemente alla successiva:
+     activeImg.next().addClass('active');
+    }
+
   })
 
-  $('.prev > i').click(function() {
-    $('.images > .active').removeClass('active').prev().addClass('active');
+  $('.prev').click(function() {
+    // creiamo una variabile per indicare l'immagine attualmente visualizzata:
+    var activeImg = $('.images img.active');
+    activeImg.removeClass('active');
+    // creiamo un if per capire se l'immagine visualizzata è la prima:
+    if (activeImg.hasClass('first')) {
+      // Se è la prima, allora l'immagine visualizzata successiva sarà l'ultima
+      var lastImg = $('.images img.last');
+      lastImg.addClass('active');
+    } else {
+      // Se non è la prima  passa semplicemente alla precedente:
+     activeImg.prev().addClass('active');
+    }
   })
-
-  if ($(".images > .last").hasClass("active")) {
-         $(".next > i").click(function () {
-         $('.images > .first').addClass('active');
-         });
-     }
